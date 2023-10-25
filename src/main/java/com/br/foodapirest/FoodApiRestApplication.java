39a510f3cc8c40f3782c5b7236c7c4195e2f7964
GabrielPiovesani <gabriel.piovesani@gmail.com>
2023-10-25 11:20:12 -0300

    primeiro commit inclusao do java e dependencias

---
+++ src/main/java/com/br/foodapirest/FoodApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.br.foodapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/foodapirest/FoodApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.foodapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
